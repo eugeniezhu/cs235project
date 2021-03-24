8b9ad38a937fd7b0e7c8c3af03e7dca80fc3b54c
nrustn <thanh.n.nguyen@sjsu.edu>
2021-03-24 12:28:58 -0700

    save user's device & time(ms) for each 'test'

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,10 +1,10 @@
+var start = 0;
+var end = 0;
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function showList() {
+  function showList() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-list");
@@ -21,8 +21,6 @@ function showList() {
   function showHue() {
     document.getElementById("myhue").classList.toggle("show");
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.huebtn')) {
       var dropdowns = document.getElementsByClassName("hue-list");
@@ -38,9 +36,7 @@ function showList() {
 
   function showHue() {
     document.getElementById("myhue").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
+  }  
   window.onclick = function(event) {
     if (!event.target.matches('.huebtn')) {
       var dropdowns = document.getElementsByClassName("hue-list");
@@ -56,9 +52,7 @@ function showList() {
 
   function showShade() {
     document.getElementById("myshade").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
+  }  
   window.onclick = function(event) {
     if (!event.target.matches('.shadebtn')) {
       var dropdowns = document.getElementsByClassName("shade-list");
@@ -74,9 +68,7 @@ function showList() {
 
   function showSaturation() {
     document.getElementById("mysaturation").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
+  }  
   window.onclick = function(event) {
     if (!event.target.matches('.saturationbtn')) {
       var dropdowns = document.getElementsByClassName("saturation-list");
@@ -91,30 +83,71 @@ function showList() {
   } 
 
   function startExp(){
-    //TODO: START TIMER HERE
-    var userDevice = document.getElementsByName('device');
-    //for DEBUG
-    for(i = 0; i < userDevice.length; i++) {
-      if(userDevice[i].checked)
-        alert(userDevice[i].value);      
-  }
-    
+    //save user's device choice
+    var d = document.getElementsByName('device');    
+    for(i = 0; i < d.length; i++) {
+      if(d[i].checked){
+        var userDevice = d[i].value;
+        sessionStorage.setItem("userD", userDevice);
+      }     
+    } 
+    //start timer for control test
+    startTime = new Date().getTime();
+    sessionStorage.setItem("startTime", startTime);     
     window.location.href = "control.html";    
   }
 
   function hue(){
+    //end timer for control test & calculate # of ms
+    endTime = new Date().getTime();
+    var controlTime = endTime - sessionStorage.getItem("startTime");
+    sessionStorage.setItem("controlTime", controlTime);     
+
     window.location.href = "hue.html";
+
+    //start timer for hue test
+    startTime = new Date().getTime();
+    sessionStorage.setItem("startTime", startTime); 
   }
 
   function shade(){
+    //end timer for hue test & calculate # of ms
+    endTime = new Date().getTime();
+    var hueTime = endTime - sessionStorage.getItem("startTime");
+    sessionStorage.setItem("hueTime", hueTime); 
+
     window.location.href = "shade.html";
+
+    //start timer for shade test
+    startTime = new Date().getTime();
+    sessionStorage.setItem("startTime", startTime);
   }
 
   function saturation(){
-    window.location.href = "saturation.html";  
+    //end timer for shade test & calculate # of ms
+    endTime = new Date().getTime();
+    var shadeTime = endTime - sessionStorage.getItem("startTime");
+    sessionStorage.setItem("shadeTime", shadeTime);
+
+    window.location.href = "saturation.html"; 
+
+    //start timer for saturation test
+    startTime = new Date().getTime();
+    sessionStorage.setItem("startTime", startTime);
   }
   
   function endExp(){    
-    //TODO END TIMER HERE
-    alert("Great job, you've completed the experiment. Thank you for your time.");
+    //end timer for saturation test & calculate # of ms
+    endTime = new Date().getTime();
+    var saturationTime = endTime - sessionStorage.getItem("startTime");
+    sessionStorage.setItem("saturationTime", saturationTime);
+    
+    // for DEBUG -- how to get variables
+    console.log("user's device: " + sessionStorage.getItem("userD"));
+    console.log("user's controlTime: " + sessionStorage.getItem("controlTime") + " ms");
+    console.log("user's hueTime: " + sessionStorage.getItem("hueTime") + " ms");
+    console.log("user's shadeTime: " + sessionStorage.getItem("shadeTime") + " ms");
+    console.log("user's saturationTime: " + sessionStorage.getItem("saturationTime") + " ms");
+    
+    alert("You've completed the experiment. Thank you so much for your time.");
   }

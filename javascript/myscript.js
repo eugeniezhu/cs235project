ea222f4c3b379775d99680a43d3d69c2f1dd5478
eugeniezhu <eugeniezhu@gmail.com>
2021-03-24 21:14:18 -0700

    generic toggle and add instructions

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,158 +1,139 @@
 var start = 0;
 var end = 0;
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-  function showControl() {
-    document.getElementById("mycontrol").classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.controlbtn')) {
-      var dropdowns = document.getElementsByClassName("control-list");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+let menus = new Map();
+menus.set('.controlbtn', 'control-list');
+menus.set('.huebtn', 'hue-list');
+menus.set('.saturationbtn', 'saturation-list');
+menus.set('.shadebtn', 'shade-list');
+
+// generic toggle menu
+function showMenu(menuName) {
+  document.getElementById(menuName).classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.controlbtn')) {
+    var dropdowns = document.getElementsByClassName("control-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
-
-  function showHue() {
-    document.getElementById("myhue").classList.toggle("show");
   }
-  window.onclick = function(event) {
-    if (!event.target.matches('.huebtn')) {
-      var dropdowns = document.getElementsByClassName("hue-list");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+} 
+
+window.onclick = function(event) {
+  if (!event.target.matches('.huebtn')) {
+    var dropdowns = document.getElementsByClassName("hue-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
+  }
+} 
 
-  function showHue() {
-    document.getElementById("myhue").classList.toggle("show");
-  }  
-  window.onclick = function(event) {
-    if (!event.target.matches('.huebtn')) {
-      var dropdowns = document.getElementsByClassName("hue-list");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.shadebtn')) {
+    var dropdowns = document.getElementsByClassName("shade-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
+  }
+} 
 
-  function showShade() {
-    document.getElementById("myshade").classList.toggle("show");
-  }  
-  window.onclick = function(event) {
-    if (!event.target.matches('.shadebtn')) {
-      var dropdowns = document.getElementsByClassName("shade-list");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.saturationbtn')) {
+    var dropdowns = document.getElementsByClassName("saturation-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
-
-  function showSaturation() {
-    document.getElementById("mysaturation").classList.toggle("show");
-  }  
-  window.onclick = function(event) {
-    if (!event.target.matches('.saturationbtn')) {
-      var dropdowns = document.getElementsByClassName("saturation-list");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  } 
-
-  function startExp(){
-    var userName = document.getElementById('userName').value;
-    sessionStorage.setItem("userN", userName)
-
-    //save user's device choice
-    var d = document.getElementsByName('device');    
-    for(i = 0; i < d.length; i++) {
-      if(d[i].checked){
-        var userDevice = d[i].value;
-        sessionStorage.setItem("userD", userDevice);
-      }     
-    } 
-    //start timer for control test
-    startTime = new Date().getTime();
-    sessionStorage.setItem("startTime", startTime);     
-    window.location.href = "control.html";    
   }
+} 
 
-  function hue(){
-    //end timer for control test & calculate # of ms
-    endTime = new Date().getTime();
-    var controlTime = endTime - sessionStorage.getItem("startTime");
-    sessionStorage.setItem("controlTime", controlTime);     
+function hue(){
+  //end timer for control test & calculate # of ms
+  endTime = new Date().getTime();
+  var controlTime = endTime - sessionStorage.getItem("startTime");
+  sessionStorage.setItem("controlTime", controlTime);     
 
-    window.location.href = "hue.html";
+  window.location.href = "hue.html";
 
-    //start timer for hue test
-    startTime = new Date().getTime();
-    sessionStorage.setItem("startTime", startTime); 
+  //start timer for hue test
+  startTime = new Date().getTime();
+  sessionStorage.setItem("startTime", startTime); 
+}
+
+function shade(){
+  //end timer for hue test & calculate # of ms
+  endTime = new Date().getTime();
+  var hueTime = endTime - sessionStorage.getItem("startTime");
+  sessionStorage.setItem("hueTime", hueTime); 
+
+  window.location.href = "shade.html";
+
+  //start timer for shade test
+  startTime = new Date().getTime();
+  sessionStorage.setItem("startTime", startTime);
+}
+
+function saturation(){
+  //end timer for shade test & calculate # of ms
+  endTime = new Date().getTime();
+  var shadeTime = endTime - sessionStorage.getItem("startTime");
+  sessionStorage.setItem("shadeTime", shadeTime);
+
+  window.location.href = "saturation.html"; 
+
+  //start timer for saturation test
+  startTime = new Date().getTime();
+  sessionStorage.setItem("startTime", startTime);
+}
+
+function startExp(){
+  var userName = document.getElementById('userName').value;
+  sessionStorage.setItem("userN", userName)
+
+  //save user's device choice
+  var d = document.getElementsByName('device');    
+  for(i = 0; i < d.length; i++) {
+    if(d[i].checked){
+      var userDevice = d[i].value;
+      sessionStorage.setItem("userD", userDevice);
+    }     
   }
+  //start timer for control test
+  startTime = new Date().getTime();
+  sessionStorage.setItem("startTime", startTime);     
+  window.location.href = "control.html";    
+}
 
-  function shade(){
-    //end timer for hue test & calculate # of ms
-    endTime = new Date().getTime();
-    var hueTime = endTime - sessionStorage.getItem("startTime");
-    sessionStorage.setItem("hueTime", hueTime); 
-
-    window.location.href = "shade.html";
-
-    //start timer for shade test
-    startTime = new Date().getTime();
-    sessionStorage.setItem("startTime", startTime);
-  }
-
-  function saturation(){
-    //end timer for shade test & calculate # of ms
-    endTime = new Date().getTime();
-    var shadeTime = endTime - sessionStorage.getItem("startTime");
-    sessionStorage.setItem("shadeTime", shadeTime);
-
-    window.location.href = "saturation.html"; 
-
-    //start timer for saturation test
-    startTime = new Date().getTime();
-    sessionStorage.setItem("startTime", startTime);
-  }
+function endExp(){    
+  //end timer for saturation test & calculate # of ms
+  endTime = new Date().getTime();
+  var saturationTime = endTime - sessionStorage.getItem("startTime");
+  sessionStorage.setItem("saturationTime", saturationTime);
   
-  function endExp(){    
-    //end timer for saturation test & calculate # of ms
-    endTime = new Date().getTime();
-    var saturationTime = endTime - sessionStorage.getItem("startTime");
-    sessionStorage.setItem("saturationTime", saturationTime);
-    
-    // for DEBUG -- how to get variables
-    console.log("user's name: " + sessionStorage.getItem("userN"));
-    console.log("user's device: " + sessionStorage.getItem("userD"));
-    console.log("user's controlTime: " + sessionStorage.getItem("controlTime") + " ms");
-    console.log("user's hueTime: " + sessionStorage.getItem("hueTime") + " ms");
-    console.log("user's shadeTime: " + sessionStorage.getItem("shadeTime") + " ms");
-    console.log("user's saturationTime: " + sessionStorage.getItem("saturationTime") + " ms");
-    
-    alert("You've completed the experiment. Thank you so much for your time.");
-  }
+  // for DEBUG -- how to get variables
+  console.log("user's name: " + sessionStorage.getItem("userN"));
+  console.log("user's device: " + sessionStorage.getItem("userD"));
+  console.log("user's controlTime: " + sessionStorage.getItem("controlTime") + " ms");
+  console.log("user's hueTime: " + sessionStorage.getItem("hueTime") + " ms");
+  console.log("user's shadeTime: " + sessionStorage.getItem("shadeTime") + " ms");
+  console.log("user's saturationTime: " + sessionStorage.getItem("saturationTime") + " ms");
+  
+  alert("You've completed the experiment. Thank you so much for your time.");
+}
